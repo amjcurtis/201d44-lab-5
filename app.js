20ce3c807a5800cfd37908c258abcb74bfd58af0
Andrew Curtis <curtis.andrewm@gmail.com>
2019-01-11 16:30:36 -0800

    Add solution to problem 3

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    var addUp = a + b;
-    var sumArray = [addUp, 'The sum of ' + a + ' and ' + b + ' is ' + addUp + '.'];
+    var addNums = a + b;
+    var messageQ1 = 'The sum of ' + a + ' and ' + b + ' is ' + addNums + '.';
+    var sumArray = [addNums, messageQ1];
     return sumArray;
 }
 
@@ -30,7 +31,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
     var multNums = a * b;
-    var multArray = [multNums, 'The product of ' + a + ' and ' + b + ' is ' + multNums + '.'];
+    var messageQ2 = 'The product of ' + a + ' and ' + b + ' is ' + multNums + '.'
+    var multArray = [multNums, messageQ2];
     return multArray;
 }
 
@@ -52,11 +54,26 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var sumOfFirstPairNums = sum(a, b)[0];
+    console.log(sumOfFirstPairNums);
+    var firstElement = sum(sumOfFirstPairNums, c)[0]; // Sum of all three args
+    console.log(firstElement);
+    var prodOfFirstPairNums = multiply(a, b)[0];
+    console.log(prodOfFirstPairNums);
+    var secondElement = multiply(prodOfFirstPairNums, c)[0]; // Product of all 3 args
+    console.log(secondElement);
+    var messageOneQ2 = a + ' and ' + b + ' and ' + c + ' sum to ' + firstElement + '.';
+    console.log(messageOneQ2);
+    var thirdElement = messageOneQ2;
+    var messageTwoQ2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + secondElement + '.';
+    console.log(messageTwoQ2);
+    var fourthElement = messageTwoQ2;
+    var sumAndMultArray = [firstElement, secondElement, thirdElement, fourthElement]; // Create array of all four elements
+    return sumAndMultArray;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
