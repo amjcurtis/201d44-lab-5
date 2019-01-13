dfedf4d592559ff328642cd4305025abe6a33ecf
amjcurtis <curtis.andrewm@gmail.com>
2019-01-13 00:03:28 -0800

    Start problem 6 stretch goal

--- app.js
+++ app.js
@@ -154,10 +154,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    var anyArray = dynamicArray;
+    for (var i = 0; i < anyArray.length; i++) {
+        var prod = multiply(anyArray[i], anyArray[i+1])[0];
+        console.log('Result of prod is', prod);
+        
+        
+    }
+    var msg = 'The numbers ' + anyArray + ' have a product of ' + + '.';
+    console.log('msg says:', msg);
+    return [, msg];// Return what??
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
