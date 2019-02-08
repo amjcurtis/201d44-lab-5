1d933e3fc2f81d00fadfea120bde19180ca1de3b
amjcurtis <curtis.andrewm@gmail.com>
2019-02-07 19:40:50 -0800

    Remove non-functional attempted solution for stretch goal problem

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    var addNums = a + b;
-    var messageQ1 = 'The sum of ' + a + ' and ' + b + ' is ' + addNums + '.';
-    var sumArray = [addNums, messageQ1];
-    return sumArray;
+  var addNums = a + b;
+  var messageQ1 = 'The sum of ' + a + ' and ' + b + ' is ' + addNums + '.';
+  var sumArray = [addNums, messageQ1];
+  return sumArray;
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -30,14 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-    var multNums = a * b;
-    var messageQ2 = 'The product of ' + a + ' and ' + b + ' is ' + multNums + '.'
-    var multArray = [multNums, messageQ2];
-    return multArray;
+  var multNums = a * b;
+  var messageQ2 = 'The product of ' + a + ' and ' + b + ' is ' + multNums + '.'
+  var multArray = [multNums, messageQ2];
+  return multArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,26 +54,26 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-    var sumOfFirstPairNums = sum(a, b)[0];
-    console.log(sumOfFirstPairNums);
-    var firstElement = sum(sumOfFirstPairNums, c)[0]; // Sum of all three args
-    console.log(firstElement);
-    var prodOfFirstPairNums = multiply(a, b)[0];
-    console.log(prodOfFirstPairNums);
-    var secondElement = multiply(prodOfFirstPairNums, c)[0]; // Product of all 3 args
-    console.log(secondElement);
-    var messageOneQ2 = a + ' and ' + b + ' and ' + c + ' sum to ' + firstElement + '.';
-    console.log(messageOneQ2);
-    var thirdElement = messageOneQ2;
-    var messageTwoQ2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + secondElement + '.';
-    console.log(messageTwoQ2);
-    var fourthElement = messageTwoQ2;
-    var sumAndMultArray = [firstElement, secondElement, thirdElement, fourthElement]; // Create array of all four elements
-    return sumAndMultArray;
+  var sumOfFirstPairNums = sum(a, b)[0];
+  console.log(sumOfFirstPairNums);
+  var firstElement = sum(sumOfFirstPairNums, c)[0]; // Sum of all three args
+  console.log(firstElement);
+  var prodOfFirstPairNums = multiply(a, b)[0];
+  console.log(prodOfFirstPairNums);
+  var secondElement = multiply(prodOfFirstPairNums, c)[0]; // Product of all 3 args
+  console.log(secondElement);
+  var messageOneQ2 = a + ' and ' + b + ' and ' + c + ' sum to ' + firstElement + '.';
+  console.log(messageOneQ2);
+  var thirdElement = messageOneQ2;
+  var messageTwoQ2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + secondElement + '.';
+  console.log(messageTwoQ2);
+  var fourthElement = messageTwoQ2;
+  var sumAndMultArray = [firstElement, secondElement, thirdElement, fourthElement]; // Create array of all four elements
+  return sumAndMultArray;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -90,14 +90,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 function sumArray(sumArr) { //eslint-disable-line
-    var inputArgs = sumArr; // Save inputted array as variable
-    var pairOne = sum(inputArgs[0], inputArgs[1])[0]; // Add first pair of numbers of inputted array arg
-    var elementOne = sum(pairOne, inputArgs[2])[0]; // Sum of all the numbers of inputted array arg
-    console.log('Sum of numbers of inputted array argument:', elementOne);
-    var msgQ4 = sumArr + ' was passed in as an array of numbers, and ' + elementOne + ' is their sum.';
-    console.log(msgQ4);
-    var sumArrayOutput = [elementOne, msgQ4];
-    return sumArrayOutput;
+  var inputArgs = sumArr; // Save inputted array as variable
+  var pairOne = sum(inputArgs[0], inputArgs[1])[0]; // Add first pair of numbers of inputted array arg
+  var elementOne = sum(pairOne, inputArgs[2])[0]; // Sum of all the numbers of inputted array arg
+  console.log('Sum of numbers of inputted array argument:', elementOne);
+  var msgQ4 = sumArr + ' was passed in as an array of numbers, and ' + elementOne + ' is their sum.';
+  console.log(msgQ4);
+  var sumArrayOutput = [elementOne, msgQ4];
+  return sumArrayOutput;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -118,13 +118,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-    var inputArg = multArr; // Save inputted array as variable
-    var pair1 = multiply(inputArg[0], inputArg[1])[0]; // Multiply first pair of numbers of inputted array arg
-    var elemOne = multiply(pair1, inputArg[2])[0]; // Product of all 3 numbers of inputted array arg
-    console.log('Product of numbers of inputted array argument:', elemOne);
-    var msgQ5 = 'The numbers ' + inputArg + ' have a product of ' + elemOne + '.';
-    var output = [elemOne, msgQ5];
-    return output;
+  var inputArg = multArr; // Save inputted array as variable
+  var pair1 = multiply(inputArg[0], inputArg[1])[0]; // Multiply first pair of numbers of inputted array arg
+  var elemOne = multiply(pair1, inputArg[2])[0]; // Product of all 3 numbers of inputted array arg
+  console.log('Product of numbers of inputted array argument:', elemOne);
+  var msgQ5 = 'The numbers ' + inputArg + ' have a product of ' + elemOne + '.';
+  var output = [elemOne, msgQ5];
+  return output;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
@@ -149,18 +149,10 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-    var anyArray = dynamicArray;
-    for (var i = 0; i < anyArray.length; i++) {
-        var prod = multiply(anyArray[i], anyArray[i+1])[0];
-        console.log('Product of ' + anyArray[i] + ' times ' +  + ' is', prod);
-       
-    }
-    var msg = 'The numbers ' + anyArray + ' have a product of ' + + '.';
-    console.log('msg says:', msg);
-    return [, msg];// Return what??
+  
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
